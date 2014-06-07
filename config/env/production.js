d68b308f62e5a510593157fdb280f208f1e78114
Mozafar <kabaros@gmail.com>
2014-06-07 18:23:10 +0100

    trying to fix production minification

--- config/env/production.js
+++ config/env/production.js
@@ -14,9 +14,11 @@ module.exports = {
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/underscore/underscore.js'
 			]
 		},
+
 		css: 'public/dist/application.min.css',
 		js: 'public/dist/application.min.js'
 	},
